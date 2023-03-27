811f9efb26bf9c740217b3e1550e220de96d4f82
raccoonboy <raccoonboy0803@gmail.com>
2023-03-28 02:15:48 +0900

    초기화  세팅

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
 import requests
 from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://raccoonboy0803:aydlalsk12@raccoonboy.9hdmenx.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('여기에 URL 입력')
 db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-
-@app.route("/movie", methods=["GET"])
-def movie_get():
-    all_movies = list(db.movies.find({},{'_id':False}))
-    return jsonify({'result':all_movies})
-    
+    return render_template('index.html')    
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
