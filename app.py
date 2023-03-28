ad669465378c0e97ab9241a850c18204349d75cc
eivomin <eivomiin@gmail.com>
2023-03-28 19:07:49 +0900

    modify files

--- app.py
+++ app.py
@@ -19,20 +19,15 @@ db = client.diary
 def home():
     return render_template('index.html')
 
-# # 게시판 조회
-# @app.route("/posts", methods=["GET"])
-# def list_posts():
-#     return render_template('')
-
 # 카테고리별 조회
-@app.route("/posts", methods=["GET"])
-def list_category():
+@app.route("/posts/<category>", methods=["GET"])
+def list_category(category):
     results = []
-    all_posts = list(db.posts.find({}))
+    all_posts = list(db.posts.find({'category':category}))
     for post in all_posts:
         post['_id'] = str(post['_id'])    ## object_id -> string으로 변환
         results.append(post)
-    return jsonify({'result':results})
+    return jsonify({'result':results})  
 
 # 게시글 상세조회
 @app.route("/posts/<category>/<p_id>", methods=["GET"])
@@ -42,24 +37,37 @@ def view_posts():
     return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
 
 # 게시글 작성
-@app.route("/posts", methods=["POST"] )
-def insert_posts():
-    name_receive = request.form['name_give']
+@app.route("/posts/<category>", methods=["POST"] )
+def insert_posts(category):
     title_receive = request.form['title_give']
     comment_receive = request.form['comment_give']
     url_receive = request.form['url_give']
-    category_receive = request.form['category_give']
+    #category_receive = request.form['category_give']
+    category_receive = category
+    name_receive = request.form['nickname_give']
     reg_date = datetime.datetime.utcnow()
     mod_date = datetime.datetime.utcnow()
+    
+    if(category_receive == "music" and category_receive == "book"):
+        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        data = requests.get(url_receive,headers=headers)
+
+        soup = BeautifulSoup(data.text, 'html.parser')
+
+        # ogtitle = soup.select_one('meta[property="og:title"]')['content'] 직접 적으면 필요X
+        ogdesc = soup.select_one('meta[property="og:description"]')['content']
+        ogimage = soup.select_one('meta[property="og:image"]')['content']  
+    else:
+        ogimage = url_receive
 
     doc = {
         'title' : title_receive,
         'comment' : comment_receive,
-        'url' : url_receive,
         'reg_date' : reg_date,
         'mod_date' : mod_date,
         'category' : category_receive,
-        'u_id' : name_receive
+        'user_id' : name_receive,
+        'image':ogimage #이미지 썸네일로 사용
     }
 
     db.posts.insert_one(doc)
@@ -72,9 +80,12 @@ def modify_posts():
     return ""
 
 # 게시글 삭제
-@app.route("/posts/<category>/<p_id>", methods=["DELETE"] )
-def delete_posts():
-    return ""
+@app.route("/posts/<p_id>", methods=["DELETE"])
+def delete_posts(p_id):        
+    
+    db.posts.delete_one({'_id':ObjectId(p_id)})
+  
+    return jsonify({'msg':'삭제완료!'}) 
 
 # 게시글에 해당하는 댓글 목록 조회
 @app.route("/posts/<category>/<p_id>/comments", methods=["GET"])
@@ -86,7 +97,7 @@ def list_comments():
 def insert_comments():
     return ""
 
-# 댓글 작성
+# 댓글 수정
 @app.route("/posts/<category>/<p_id>/comments/<r_id>", methods=["PUT"])
 def modify_comments():
     return ""
