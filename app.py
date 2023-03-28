9e260d0583854b398083c1d4e3bd7c8aafd31233
eivomin <eivomiin@gmail.com>
2023-03-28 16:49:10 +0900

    add rest api

--- app.py
+++ app.py
@@ -14,53 +14,83 @@ ca = certifi.where()
 client = MongoClient('mongodb://spart:test@ac-ywf5hos-shard-00-00.vcctwzu.mongodb.net:27017,ac-ywf5hos-shard-00-01.vcctwzu.mongodb.net:27017,ac-ywf5hos-shard-00-02.vcctwzu.mongodb.net:27017/?ssl=true&replicaSet=atlas-1f9jmh-shard-0&authSource=admin&retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.diary
 
-# 메인페이지
+# 메인 페이지
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 등록
-# @app.route("/insert", methods=["POST"])
-# def movie_post():
-#     title_receive = request.form['title_give']
-#     desc_receive = request.form['desc_give']
-#     url_receive = request.form['url_give']
-#     category_receive = request.form['category_give']
-#     nickname_receive = request.form['nickname_give']
-#     reg_date = datetime.datetime.utcnow()
-#     mod_date = datetime.datetime.utcnow()
-#     ## file_upload
+# 게시판 조회
+@app.route("/posts", methods=["GET"])
+def list_posts():
+    return render_template('')
 
-#     doc = {
-#         'title' : title_receive,
-#         'desc' : desc_receive,
-#         'url' : url_receive,
-#         'reg_date' : reg_date,
-#         'mod_date' : mod_date,
-#         'category' : category_receive,
-#         'nickname' : nickname_receive
-#     }
+# 카테고리별 조회
+@app.route("/posts/<category>", methods=["GET"])
+def list_category():
+    results = []
+    all_movies = list(db.movies.find({}))
+    for movie in all_movies:
+        movie['_id'] = str(movie['_id'])    ## object_id -> string으로 변환
+        results.append(movie)
+    return jsonify({'result':results})
 
-#     db.movies.insert_one(doc)
+# 게시글 상세조회
+@app.route("/posts/<category>/<p_id>", methods=["GET"])
+def view_posts():
+    # p_id 값 가져오기
+    p_id_receive = request.args.get('p_id')
+    return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
 
-#     return jsonify({'msg':'저장완료!'})
+# 게시글 작성
+@app.route("/posts/<category>", methods=["POST"] )
+def insert_posts():
+    title_receive = request.form['title_give']
+    desc_receive = request.form['desc_give']
+    url_receive = request.form['url_give']
+    category_receive = request.form['category_give']
+    nickname_receive = request.form['nickname_give']
+    reg_date = datetime.datetime.utcnow()
+    mod_date = datetime.datetime.utcnow()
+    ## file_upload
 
-# # 목록 불러오기
-# @app.route("/movie", methods=["GET"])
-# def movie_get():
-#     results = []
-#     all_movies = list(db.movies.find({}))
-#     for movie in all_movies:
-#         movie['_id'] = str(movie['_id'])    ## object_id -> string으로 변환
-#         results.append(movie)
-#     return jsonify({'result':results})
+    doc = {
+        'title' : title_receive,
+        'desc' : desc_receive,
+        'url' : url_receive,
+        'reg_date' : reg_date,
+        'mod_date' : mod_date,
+        'category' : category_receive,
+        'nickname' : nickname_receive
+    }
 
-# # 상세조회
-# @app.route("/view", methods=["GET"])
-# def view():
-#     # p_id 값 가져오기
-#     p_id_receive = request.args.get('p_id')
-#     return jsonify({'result':'success', 'msg': '이 요청은 GET!'})
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg':'저장완료!'})
+
+# 게시글 수정
+@app.route("/posts/<category>/<p_id>", methods=["PUT"] )
+def modify_posts():
+    return ""
+
+# 게시글 삭제
+@app.route("/posts/<category>/<p_id>", methods=["DELETE"] )
+def delete_posts():
+    return ""
+
+# 게시글에 해당하는 댓글 목록 조회
+@app.route("/posts/<category>/<p_id>/comments", methods=["GET"])
+def list_comments():
+    return ""
+
+# 댓글 작성
+@app.route("/posts/<category>/<p_id>/comments", methods=["POST"])
+def insert_comments():
+    return ""
+
+# 댓글 삭제
+@app.route("/posts/<category>/<p_id>/comments/<r_id>", methods=["DELETE"])
+def delete_comments():
+    return ""
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
